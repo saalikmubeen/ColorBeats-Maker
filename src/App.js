40722c86a54c6d5268a4de4ca4d00aae1c6f2412
saalik123 <63722568+saalik123@users.noreply.github.com>
2020-09-21 20:22:03 +0530

    initial commit - made sounds to work

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Sounds from "./Sounds";
+
+
+class App extends Component {
+
+  render () {
+    return(
+        <Sounds/>
+    )
+  }
+}
+
+export default App;
---
+++ src/Sounds.js
@@ -0,0 +1,162 @@
+import React, { Component } from 'react';
+import ReactHowler from 'react-howler';
+
+import bubbles from "./sounds/bubbles.mp3";
+import clay from "./sounds/clay.mp3";
+import confetti from "./sounds/confetti.mp3";
+import corona from "./sounds/corona.mp3";
+import spiral from "./sounds/dotted-spiral.mp3";
+import flash1 from "./sounds/flash-1.mp3";
+import flash2 from "./sounds/flash-2.mp3";
+import flash3 from "./sounds/flash-3.mp3";
+import glimmer from "./sounds/glimmer.mp3";
+import moon from "./sounds/moon.mp3";
+import pinwheel from "./sounds/pinwheel.mp3";
+import piston1 from "./sounds/piston-1.mp3";
+import piston2 from "./sounds/piston-2.mp3";
+import piston3 from "./sounds/piston-3.mp3";
+import prism1 from './sounds/prism-1.mp3';
+import prism2 from "./sounds/prism-2.mp3";
+import prism3 from "./sounds/prism-3.mp3";
+import splits from "./sounds/splits.mp3";
+import squiggle from './sounds/squiggle.mp3';
+import strike from "./sounds/strike.mp3";
+import suspension from "./sounds/suspension.mp3";
+import timer from "./sounds/timer.mp3";
+import ufo from "./sounds/ufo.mp3";
+import veil from "./sounds/veil.mp3";
+import wipe from "./sounds/wipe.mp3";
+import zigZag from "./sounds/zig-zag.mp3";
+
+
+class Sounds extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            sound: bubbles
+        }
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+    }
+
+    handleKeyPress(e){
+        
+        if(e.keyCode === 65){
+            this.setState({sound: bubbles})
+        }
+
+        if(e.keyCode === 66){
+            this.setState({sound: clay})
+        }
+
+        if(e.keyCode === 67){
+            this.setState({sound: confetti})
+        }
+
+        if(e.keyCode === 68){
+            this.setState({sound: corona})
+        }
+
+        if(e.keyCode === 69){
+            this.setState({sound: spiral})
+        }
+
+        if(e.keyCode === 70){
+            this.setState({sound: flash1})
+        }
+
+        if(e.keyCode === 71){
+            this.setState({sound: flash2})
+        }
+
+        if(e.keyCode === 72){
+            this.setState({sound: flash3})
+        }
+
+        if(e.keyCode === 73){
+            this.setState({sound: glimmer})
+        }
+
+        if(e.keyCode === 74){
+            this.setState({sound: moon})
+        }
+
+        if(e.keyCode === 75){
+            this.setState({sound: pinwheel})
+        }
+
+        if(e.keyCode === 76){
+            this.setState({sound: piston1})
+        }
+
+        if(e.keyCode === 77){
+            this.setState({sound: piston2})
+        }
+
+        if(e.keyCode === 78){
+            this.setState({sound: piston3})
+        }
+
+        if(e.keyCode === 79){
+            this.setState({sound: prism1})
+        }
+
+        if(e.keyCode === 80){
+            this.setState({sound: prism2})
+        }
+
+        if(e.keyCode === 81){
+            this.setState({sound: prism3})
+        }
+
+        if(e.keyCode === 82){
+            this.setState({sound: splits})
+        }
+
+        if(e.keyCode === 83){
+            this.setState({sound: squiggle})
+        }
+
+        if(e.keyCode === 84){
+            this.setState({sound: strike})
+        }
+
+        if(e.keyCode === 85){
+            this.setState({sound: suspension})
+        }
+
+        if(e.keyCode === 86){
+            this.setState({sound: timer})
+        }
+
+        if(e.keyCode === 87){
+            this.setState({sound: ufo})
+        }
+
+        if(e.keyCode === 88){
+            this.setState({sound: veil})
+        }
+
+        if(e.keyCode === 89){
+            this.setState({sound: wipe})
+        }
+
+        if(e.keyCode === 90){
+            this.setState({sound: zigZag})
+        }
+    }
+
+    render() {
+         var {sound} = this.state;
+        return (
+             <div onKeyDown={this.handleKeyPress} tabIndex={0} style={{position: "absolute", height: "100vh", width: "100%"}}>
+                 <ReactHowler
+                 src={sound}
+                 playing={true}
+                 />
+             </div>
+        )
+    }
+}
+
+
+export default Sounds;
