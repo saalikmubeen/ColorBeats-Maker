3dfbccc4f63eefef32c1fcc566bbd760a0daa11f
saalik123 <63722568+saalik123@users.noreply.github.com>
2020-09-22 17:39:01 +0530

    added more animations

--- src/Sounds.js
+++ src/Sounds.js
@@ -67,7 +67,7 @@ class Sounds extends Component {
         colors: [
              a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
         ],
-        animations: ["animate-side", "animate-up"]
+        animations: ["animate-left", "animate-up", "animate-right", "animate-down", "animate-size"]
     }
 
     constructor(props){
